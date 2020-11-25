bd9d03c34fd0bd802b123def674c0b4edc92e81e
vishnuu95 <vishnuu@terpmail.umd.edu>
2020-11-25 03:41:33 -0500

    Declared object detector class for object detection and avoidance

---
+++ Gazebo_tutorials/include/detectObstacle.hpp
@@ -0,0 +1,30 @@
+/** @file detecetObstacle.cpp
+* @brief Obstacle detector class definition
+* @Copyright MIT License 2020 Vishnuu
+*/
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/Twist.h>
+#include <iostream>
+
+/**
+* @brief detectObstacle Class for obstacle detection, 
+* command publish, scan laser topic
+*/
+class detectObstacle {
+ public:
+ 	float omega, vel;
+ 	float threshold;
+ 	bool isNearObs;
+ 	geometry_msgs::Twist msg;
+ 	ros::Publisher myPublisher;
+ 	ros::Subscriber mySubscriber;
+ 	ros::NodeHandle n;
+ 	int rightFov;
+ 	int leftFov;
+	detectObstacle(float, float, float);
+	~detectObstacle();
+	void scanSpace(const sensor_msgs::LaserScan::ConstPtr& messg);
+	void moveForward();
+	bool isWithinObsRange();
+};
