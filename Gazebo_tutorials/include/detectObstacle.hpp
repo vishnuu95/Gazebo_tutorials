7f81f4c72241bd0430ba5aa713b8c180f82299a0
vishnuu95 <vishnuu@terpmail.umd.edu>
2020-11-25 11:06:08 -0500

    modified code from static testing

--- Gazebo_tutorials/include/detectObstacle.hpp
+++ Gazebo_tutorials/include/detectObstacle.hpp
@@ -2,6 +2,9 @@
 * @brief Obstacle detector class definition
 * @Copyright MIT License 2020 Vishnuu
 */
+#ifndef GAZEBO_TUTORIALS_INCLUDE_DETECTOBSTACLE_HPP_
+#define GAZEBO_TUTORIALS_INCLUDE_DETECTOBSTACLE_HPP_
+
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
@@ -13,18 +16,20 @@
 */
 class detectObstacle {
  public:
- 	float omega, vel;
- 	float threshold;
- 	bool isNearObs;
- 	geometry_msgs::Twist msg;
- 	ros::Publisher myPublisher;
- 	ros::Subscriber mySubscriber;
- 	ros::NodeHandle n;
- 	int rightFov;
- 	int leftFov;
-	detectObstacle(float, float, float);
-	~detectObstacle();
-	void scanSpace(const sensor_msgs::LaserScan::ConstPtr& messg);
-	void moveForward();
-	bool isWithinObsRange();
+    float omega, vel;
+    float threshold;
+    bool isNearObs;
+    geometry_msgs::Twist msg;
+    ros::Publisher myPublisher;
+    ros::Subscriber mySubscriber;
+    ros::NodeHandle n;
+    int rightFov;
+    int leftFov;
+    detectObstacle(float, float, float);
+    ~detectObstacle();
+    void scanSpace(const sensor_msgs::LaserScan::ConstPtr& messg);
+    void moveForward();
+    bool isWithinObsRange();
 };
+
+#endif  // GAZEBO_TUTORIALS_INCLUDE_DETECTOBSTACLE_HPP_
--- Gazebo_tutorials/src/detectObstacle.cpp
+++ Gazebo_tutorials/src/detectObstacle.cpp
@@ -14,15 +14,17 @@
 * @return None
 */
 detectObstacle::detectObstacle(float threshold, float vel, float omega) {
-	this->threshold = threshold;
-	this->vel = vel;
-	this->omega = omega;
-	// this->isWithinObsRange = false;
-	this->msg;
-	this->myPublisher = this->n.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
-	this->mySubscriber = this->n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, &detectObstacle::scanSpace, this);
-	this->rightFov = 65;
-	this->leftFov = 65;
+    this->threshold = threshold;
+    this->vel = vel;
+    this->omega = omega;
+    // this->isWithinObsRange = false;
+    this->msg;
+    this->myPublisher = this->n.advertise<geometry_msgs::Twist>
+    ("/cmd_vel", 1000);
+    this->mySubscriber = this->n.subscribe<sensor_msgs::LaserScan>
+    ("/scan", 1000, &detectObstacle::scanSpace, this);
+    this->rightFov = 65;
+    this->leftFov = 65;
 }
 
 /**
@@ -31,13 +33,13 @@ detectObstacle::detectObstacle(float threshold, float vel, float omega) {
 * @return - None
 */
 detectObstacle::~detectObstacle() {
-	this->msg.linear.x = 0;
-	this->msg.linear.y = 0;
-	this->msg.linear.z = 0;
-	this->msg.angular.x = 0;
-	this->msg.angular.y = 0;
-	this->msg.angular.z = 0;
-	this->myPublisher.publish(this->msg);
+    this->msg.linear.x = 0;
+    this->msg.linear.y = 0;
+    this->msg.linear.z = 0;
+    this->msg.angular.x = 0;
+    this->msg.angular.y = 0;
+    this->msg.angular.z = 0;
+    this->myPublisher.publish(this->msg);
 }
 
 /**
@@ -46,24 +48,24 @@ detectObstacle::~detectObstacle() {
 * @return None
 */
 void detectObstacle::scanSpace(const sensor_msgs::LaserScan::ConstPtr& msg) {
-	// Check if obstacle is present in front right side
-	for(auto i = 0; i  < this->rightFov; i ++) {
-		if(msg->ranges[i]  < this->threshold) {
-			this->isNearObs = true;
-			ROS_WARN_STREAM("Obstacle is ahead");
-			return;
-		}
-	}
-	// Check if obstacle is present in front left side
-	for(auto i = this->leftFov; i < 360; i++){
-		if(msg->ranges[i] < this->threshold) {
-			this->isNearObs = true;
-			ROS_WARN_STREAM("Obstacle is ahead");
-			return;
-		}
-	}
-	this->isNearObs = false;
-	return;
+    // Check if obstacle is present in front right side
+    for (auto i = 0; i  < this->rightFov; i ++) {
+        if (msg->ranges[i]  < this->threshold) {
+            this->isNearObs = true;
+            ROS_WARN_STREAM("Obstacle is ahead");
+            return;
+        }
+    }
+    // Check if obstacle is present in front left side
+    for (auto i = this->leftFov; i < 360; i++) {
+        if (msg->ranges[i] < this->threshold) {
+            this->isNearObs = true;
+            ROS_WARN_STREAM("Obstacle is ahead");
+            return;
+        }
+    }
+    this->isNearObs = false;
+    return;
 }
 
 
@@ -73,21 +75,20 @@ void detectObstacle::scanSpace(const sensor_msgs::LaserScan::ConstPtr& msg) {
 * @return None
 */
 void detectObstacle::moveForward() {
-	while (1){
-		if (ros::ok()) {
-			// if (this->isWithinObsRange()) {
-			// 	msg.linear.x = 0;
-			// 	this->msg.angular.z = this->omega;
-			// } else {
-			// 	this->msg.linear.x = this->vel;
-			// 	this->msg.linear.z = 0;
-			// }
-			this->msg.linear.x=this->vel;
-			this->myPublisher.publish(this->msg);
-			ros::spinOnce();
-		}
-
-	}
+    while (1) {
+        if (ros::ok()) {
+            // if (this->isWithinObsRange()) {
+            //  msg.linear.x = 0;
+            //  this->msg.angular.z = this->omega;
+            // } else {
+            //  this->msg.linear.x = this->vel;
+            //  this->msg.linear.z = 0;
+            // }
+            this->msg.linear.x = this->vel;
+            this->myPublisher.publish(this->msg);
+            ros::spinOnce();
+        }
+    }
 }
 
 /**
@@ -96,7 +97,7 @@ void detectObstacle::moveForward() {
 * @return bool value
 */
 bool detectObstacle::isWithinObsRange() {
-	return this->isNearObs;
+    return this->isNearObs;
 }
 
 /**
@@ -105,7 +106,7 @@ bool detectObstacle::isWithinObsRange() {
 * return None
 */
 int main(int argc, char* argv[]) {
-	ros::init(argc, argv, "obsdetector");
-	detectObstacle myRobot(0.5, 1, 0.2);
-	myRobot.moveForward();
+    ros::init(argc, argv, "obsdetector");
+    detectObstacle myRobot(0.5, 1, 0.2);
+    myRobot.moveForward();
 }
